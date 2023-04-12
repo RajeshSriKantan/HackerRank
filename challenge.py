8ae016a6a23c965d4e6c072cb74002a1163a2528
RSK <129513356+RajeshSriKantan@users.noreply.github.com>
2023-04-12 18:41:54 +0530

    chaleenge4-1.2

--- challenge.py
+++ challenge.py
@@ -51,3 +51,8 @@ if __name__ == '__main__':
     n = int(input())
     for i in range(1,4):
         print(i,end="")
+#anothermethod(4)
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,n+1):
+        print(i,end="")
