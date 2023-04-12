2c0c6191666f3693a8b0960ff5eb035d19b021de
RSK <129513356+RajeshSriKantan@users.noreply.github.com>
2023-04-12 17:59:21 +0530

    challenge3

--- challenge.py
+++ challenge.py
@@ -25,3 +25,14 @@ f __name__ == '__main__':
     print(c)
     print(d)
     print(e)
+
+    
+    #The provided code stub reads and integer, , from STDIN. For all non-negative integers , print .
+#Example
+#The list of non-negative integers that are less than  is . Print the square of each number on a separate line.
+if __name__ == '__main__':
+    n = int(input())
+    i=0
+    while(i<n):
+        print(i*i)
+        i+=1
