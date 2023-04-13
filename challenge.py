9cf1d563577b8d61f3c789d4262cb98f0b913d76
RSK <129513356+RajeshSriKantan@users.noreply.github.com>
2023-04-13 18:27:02 +0530

    challenge5

--- challenge.py
+++ challenge.py
@@ -56,3 +56,21 @@ if __name__ == '__main__':
     n = int(input())
     for i in range(1,n+1):
         print(i,end="")
+
+        #(5)Task 
+#Given an integer, , perform the following conditional actions:
+
+#If n is odd, print Weird
+#If n is even and in the inclusive range of 2 to 5, print Not Weird
+#If n is even and in the inclusive range of  6to 20, print Weird
+I#f n is even and greater than 20, print Not Weird
+
+if(n%2!=0):
+    print("Weird")
+else:
+    if(n in range(2,6)):
+        print("Not Weird")
+    elif(n in range(6,21)):
+        print("Weird")
+    else:
+        print("Not Weird")
